877792613981881ced41e91d46951884f1f9c7f4
Poire Molle <s224311@dtu.dk>
2023-03-08 22:52:13 +0100

    splitting message in plain text and image headers

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         crudeClient client = new crudeClient();
-        //client.setMsgSequence("info@comit.dev", "s224311@dtu.dk", "test");
+        client.setMsgSequence("info@comit.dev", "s224311@dtu.dk", "test");
+        client.attachImage("src/clippy.jpg");
         //client.sendMail();
         client.sendImage();
     }
