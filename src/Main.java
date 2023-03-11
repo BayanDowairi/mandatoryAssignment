e4de7ffb60e02ac29499341982acbdfcafd6452c
NegarOstad <s224283@student.dtu.dk>
2023-03-11 14:21:58 +0100

    working on sending gmail

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+        crudeClient client = new crudeClient();
+        client.setClientSequence("bayouna96@gmail.com" , "nr.salmani@gmail.com" ,"Hiiiiiii");
+        client.sendMail();
     }
 }
--- src/crudeClient.java
+++ src/crudeClient.java
@@ -1,3 +1,4 @@
+import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 import javax.net.ssl.*;
 import java.io.*;
@@ -9,20 +10,28 @@ public class crudeClient {
     public static String pass = "qzjiclxjatriavwz";
     private static DataOutputStream dataOutputStream;
     public static BufferedReader br = null;
+    private String message;
+    String[] clientResponse = new String[10];
+    //SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+    String username = Base64.getEncoder().encodeToString(user.getBytes(StandardCharsets.UTF_8));
+    String password = Base64.getEncoder().encodeToString(pass.getBytes(StandardCharsets.UTF_8));
+    int delay = 1000;
 
-    public static void main(String[] args) throws Exception {
-        int delay = 1000;
 
-        String username = Base64.getEncoder().encodeToString(user.getBytes(StandardCharsets.UTF_8));
-        String password = Base64.getEncoder().encodeToString(pass.getBytes(StandardCharsets.UTF_8));
-        SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-        SSLSocket sslSocket = (SSLSocket) sslSocketFactory.createSocket("smtp.gmail.com", 465);
+    //public static void main(String[] args) throws Exception {
+        //int delay = 1000;
 
-        br = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
+        //String username = Base64.getEncoder().encodeToString(user.getBytes(StandardCharsets.UTF_8));
+        //String password = Base64.getEncoder().encodeToString(pass.getBytes(StandardCharsets.UTF_8));
 
-        dataOutputStream = new DataOutputStream(sslSocket.getOutputStream());
+        //SSLSocket sslSocket = (SSLSocket) sslSocketFactory.createSocket("smtp.gmail.com", 465);
 
-        send("EHLO smtp.gmail.com\r\n",9);
+       // br = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
+
+        //dataOutputStream = new DataOutputStream(sslSocket.getOutputStream());
+
+
+        /*send("EHLO smtp.gmail.com\r\n",9);
         send("AUTH LOGIN\r\n",1);
         send(username+"\r\n",1);
         send(password+"\r\n",1);
@@ -32,9 +41,10 @@ public class crudeClient {
         send("Subject: Email test\r\n",0);
         send("Email Body\r\n",0);
         send(".\r\n",0);
-        send("QUIT\r\n",1);
-    }
-    private static void send(String s, int no_of_response_line) throws Exception
+        send("QUIT\r\n",1);*/
+    //}
+
+    /*private static void send(String s, int no_of_response_line) throws Exception
     {
         dataOutputStream.writeBytes(s);
         System.out.println("CLIENT: "+s);
@@ -44,6 +54,100 @@ public class crudeClient {
         for (int i = 0; i < no_of_response_line; i++) {
             System.out.println("SERVER : " +br.readLine());
         }
+    }*/
+    public void setClientSequence(String sender, String recieiver, String message) throws IOException {
+        recieiver = Base64.getEncoder().encodeToString(user.getBytes(StandardCharsets.UTF_8));
+        this.message = message;
+        this.clientResponse[0] = "AUTH LOGIN\r\n";
+        this.clientResponse[1] = "YmF5b3VuYTk2QGdtYWlsLmNvbQ==\r\n";
+        this.clientResponse[2] = "cXpqaWNseGphdHJpYXZ3eg==\r\n";
+        this.clientResponse[3] = "MAIL FROM: " + "<" + sender + ">";
+        this.clientResponse[4] = "RCPT TO:" + "<" + recieiver + ">";
+        this.clientResponse[5] = "DATA\r\n";
+        this.clientResponse[6] = "Subject: Email test\r\n";
+        this.clientResponse[7] = "Email Body: " + message;
+        this.clientResponse[8] = ".\r\n";
+        this.clientResponse[9] = "QUIT\r\n";
+
+
+    }
+    public void sendMail(){
+        try {
+            SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+                SSLSocket sslSocket = (SSLSocket) sslSocketFactory.createSocket("smtp.gmail.com", 465);
+               BufferedReader reader = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
+
+                DataOutputStream dataOutputStream = new DataOutputStream(sslSocket.getOutputStream());
+                String s , serverMsg;
+
+             s = "EHLO smtp.gmail.com\r\n";
+            System.out.println("Client: " + s);
+            dataOutputStream.writeBytes(s);
+            Thread.sleep(1000);
+
+            serverMsg = reader.readLine(); // read from server
+            System.out.println("Server:" + serverMsg);
+
+            serverMsg = reader.readLine();
+            System.out.println("Server:" + serverMsg);
+
+            serverMsg = reader.readLine();
+            System.out.println("Server:" + serverMsg);
+
+            serverMsg = reader.readLine();
+            System.out.println("Server:" + serverMsg);
+
+            serverMsg = reader.readLine();
+            System.out.println("Server:" + serverMsg);
+
+            serverMsg = reader.readLine();
+            System.out.println("Server:" + serverMsg);
+
+            serverMsg = reader.readLine();
+            System.out.println("Server:" + serverMsg);
+
+            serverMsg = reader.readLine();
+            System.out.println("Server:" + serverMsg);
+
+            serverMsg = reader.readLine();
+            System.out.println("Server:" + serverMsg);
+
+
+            /*for (int i = 0; i < 10; i++) {
+                serverMsg = reader.readLine();
+                System.out.println("Server:" + serverMsg);
+            }*/
+            /*s = "AUTH LOGIN\r\n";
+            System.out.println("Client: " + s);
+            dataOutputStream.writeBytes(s);
+            Thread.sleep(1000);
+            serverMsg = reader.readLine();
+            System.out.println("Server:" + serverMsg);*/
+
+
+
+            for (int i = 0; i < clientResponse.length; i++) {
+                s = clientResponse[i];
+                System.out.println("Client:" + s);
+                dataOutputStream.writeBytes(s);
+                Thread.sleep(1000);
+                serverMsg = reader.readLine();
+                System.out.println("Server:" + serverMsg);
+                //Thread.sleep(1000);
+                dataOutputStream.flush();
+            }
+            /*serverMsg = reader.readLine();
+            System.out.println("Server:" + serverMsg);
+
+            /*s = "RCPT TO:<nr.salmani@gmail.com>";
+            System.out.println("Client: " + s);
+            dataOutputStream.writeBytes(s);
+            Thread.sleep(1000);
+            serverMsg = reader.readLine();
+            System.out.println("Server:" + serverMsg);*/
+
+
+            } catch (Exception e){e.printStackTrace();}
+    }
     }
 
-}
