024843943c1c8d5bc1dec7bbf09d22439f50aa19
Bayan <113907779+BayanDowairi@users.noreply.github.com>
2023-03-11 10:19:38 +0100

    comment

--- src/crudeClient.java
+++ src/crudeClient.java
@@ -11,7 +11,7 @@ public class crudeClient {
     public static BufferedReader br = null;
 
     public static void main(String[] args) throws Exception {
-        int delay = 1000;
+       // int delay = 1000;
 
         String username = Base64.getEncoder().encodeToString(user.getBytes(StandardCharsets.UTF_8));
         String password = Base64.getEncoder().encodeToString(pass.getBytes(StandardCharsets.UTF_8));
@@ -22,28 +22,28 @@ public class crudeClient {
 
         dataOutputStream = new DataOutputStream(sslSocket.getOutputStream());
 
-        send("EHLO smtp.gmail.com\r\n",9);
-        send("AUTH LOGIN\r\n",1);
-        send(username+"\r\n",1);
-        send(password+"\r\n",1);
-        send("MAIL FROM:<bayouna96@gmail.com>\r\n",1);
-        send("RCPT TO:<nr.salmani@gmail.com>\r\n",1);
-        send("DATA\r\n",1);
-        send("Subject: Email test\r\n",0);
-        send("Email Body\r\n",0);
-        send(".\r\n",0);
-        send("QUIT\r\n",1);
+        send("EHLO smtp.gmail.com\r\n"/*9*/);
+        send("AUTH LOGIN\r\n"/*1*/);
+        send(username+"\r\n"/*1*/);
+        send(password+"\r\n"/*1*/);
+        send("MAIL FROM:<bayouna96@gmail.com>\r\n"/*1*/);
+        send("RCPT TO:<nr.salmani@gmail.com>\r\n"/*1*/);
+        send("DATA\r\n"/*1*/);
+        send("Subject: Email test\r\n"/*1*/);
+        send("Email Body\r\n"/*1*/);
+        send(".\r\n"/*1*/);
+        send("QUIT\r\n"/*1*/);
     }
-    private static void send(String s, int no_of_response_line) throws Exception
+    private static void send(String s/*int no_of_response_line*/) throws Exception
     {
         dataOutputStream.writeBytes(s);
         System.out.println("CLIENT: "+s);
-        Thread.sleep(1000);
+        //Thread.sleep(1000);
 
         // Just reading the number of lines the server will respond.
-        for (int i = 0; i < no_of_response_line; i++) {
-            System.out.println("SERVER : " +br.readLine());
-        }
+       // for (int i = 0; i < no_of_response_line; i++) {
+         //   System.out.println("SERVER : " +br.readLine());
+        //}
     }
 
 }
